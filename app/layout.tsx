a3d415075263a46a83a489e854356d95c1040327
tamim-18 <tamim11903060@gmail.com>
2024-04-02 07:21:46 +0600

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
       <html lang="en">
         {/* <CrispProvider /> */}
         <Chatbot />
+        {/* This is for chatbot */}
         <body className={inter.className}>{children}</body>
       </html>
     </ClerkProvider>
